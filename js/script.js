2dc8df313213d2a070a8900f7e2d7d3e8b98be1c
Mim <fatemamim28@gmail.com>
2021-08-03 11:13:54 +0600

    ff

--- js/script.js
+++ js/script.js
@@ -39,3 +39,18 @@ $('.banner-slide').slick({
     ]
 
 });
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop()>100){
+            $('#scrolltop').fadeIn();
+        }
+        else{
+            $('#scrolltop').fadeOut();
+        }
+    });
+    $('#scrolltop').click(function(){
+        $('html,body').animate({
+            scrollTop:0
+        },5000);
+    })
+});
