607dad3920722bc7ec8b3d50c311bb270e764dd7
Mim <fatemamim28@gmail.com>
2021-08-13 15:06:40 +0600

    all done

--- js/script.js
+++ js/script.js
@@ -8,24 +8,42 @@ $('.banner-slide').slick({
     prevArrow: '<i class="fas fa-angle-left prev"></i>',
     nextArrow: '<i class="fas fa-angle-right next"></i>',
     responsive: [
+        // {
+        //     breakpoint: 1024,
+        //     settings: {
+        //         slidesToShow: 3,
+        //         slidesToScroll: 3,
+        //         infinite: true,
+        //         dots: true
+        //     }
+        // },
+
         {
-            breakpoint: 1024,
+            breakpoint: 1199.98,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
             }
         },
         {
-            breakpoint: 600,
+            breakpoint: 991.98,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
             }
         },
         {
-            breakpoint: 576,
+            breakpoint: 767.98,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 575.98,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
